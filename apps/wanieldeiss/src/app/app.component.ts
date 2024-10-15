c994da7da4a2552552bf021233156e532e1f3b8a
Daniel Weiß <daniel.weiss@grollmus.de>
2024-10-15 14:20:48 +0200

    remove unused stylesheet

--- apps/wanieldeiss/src/app/app.component.ts
+++ apps/wanieldeiss/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { DarkModeService } from './services/dark-mode.service';
 @Component({
   selector: 'wd-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   isDarkModeEnabled$ = this.darkModeService.isDarkModeEnabled$;
